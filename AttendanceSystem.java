b4dd7210946fa4575628ab5c53223c5f92ed01e2
Rilaneh Marie L. Acabal <236932192+RilanehMarie@users.noreply.github.com>
2026-09-21 12:20:53 +0800

    Add AttendanceSystem.java file

--- AttendanceSystem.java
+++ AttendanceSystem.java
@@ -1,4 +1,4 @@
-// Adaptee - the existing attendance tracking system with an incompatible interface
+
 public class AttendanceSystem {
     public void markAttendance() {
         System.out.println("Attendance has been marked successfully.");
