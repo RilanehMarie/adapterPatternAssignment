8ce65b3b781fad3157f6ad2086ac6d43abc5acdb
Rilaneh Marie L. Acabal <236932192+RilanehMarie@users.noreply.github.com>
2026-09-21 12:21:32 +0800

    Update and rename 5.java.java to LibrarySystem.java

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -1,4 +1,4 @@
-// Adaptee - the existing library management system with an incompatible interface
+
 public class LibrarySystem {
     public void manageBooks() {
         System.out.println("Library books have been managed successfully.");
