6fe6f8da549073f09b8f7abef34c12b88bcdcc8f
Rilaneh Marie L. Acabal <236932192+RilanehMarie@users.noreply.github.com>
2026-09-21 12:20:30 +0800

    Rename SchoolManagementDemo to Main

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 
-public class SchoolManagementDemo {
+public class Main {
     public static void main(String[] args) {
        
         AttendanceSystem attendanceSystem = new AttendanceSystem();
