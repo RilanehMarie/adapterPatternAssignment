99a515b5930f345ba73ace15c53a48ad1afe6a49
Rilaneh Marie L. Acabal <236932192+RilanehMarie@users.noreply.github.com>
2026-09-21 12:21:14 +0800

    Update and rename 6.java.java to GradingSystem.java

--- GradingSystem.java
+++ GradingSystem.java
@@ -1,4 +1,4 @@
-// Adaptee - the existing grading system with an incompatible interface
+
 public class GradingSystem {
     public void recordGrades() {
         System.out.println("Grades have been recorded successfully.");
